e33b9b68b2c4e285c1d699ba1282ef9f55c22bee
DrAmanJy <dramanjy0770@gmail.com>
2026-03-25 22:45:42 +0530

    feat(api): implement database connection and improve server startup error handling

--- apps/api/src/server.js
+++ apps/api/src/server.js
@@ -1,6 +1,18 @@
-import app from "./app.js";
+import app from './app.js';
+import { connectDb } from './config/db.js';
 
-app.listen(3000, (err) => {
-  if (err) console.error(err.message);
-  console.log("Server is running on http://localhost:3000");
-});
+const PORT = process.env.PORT;
+
+(async () => {
+  try {
+    await connectDb();
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  } catch (error) {
+    console.error(
+      'Critical: Server failed to start due to DB connection error.',
+    );
+    process.exit(1);
+  }
+})();
