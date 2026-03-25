53fdb30bd7d49e471c42a05f70e38e9ace208eb9
DrAmanJy <dramanjy0770@gmail.com>
2026-03-25 22:44:29 +0530

    refactor(app): standardize string quotes and integrate authentication routes

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -1,9 +1,12 @@
-import e from "express";
+import e from 'express';
+import authRoutes from './modules/auth/auth.routes.js';
 
 const app = e();
 
-app.use(e.json({ limit: "1kb" }));
+app.use(e.json({ limit: '1kb' }));
 
-app.get("/", (req, res) => res.json({ message: "hello world" }));
+app.get('/', (req, res) => res.json({ message: 'hello world' }));
+
+app.use('/api/v1/auth', authRoutes);
 
 export default app;
