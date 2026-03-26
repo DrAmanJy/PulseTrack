2201bcee52f49c2e4e33484a5f26954432d1985b
DrAmanJy <dramanjy0770@gmail.com>
2026-03-26 22:19:19 +0530

    feat(token): add utility function to generate JWT tokens for access and refresh types

---
+++ apps/api/src/shared/utils/generateToken.js
@@ -0,0 +1,15 @@
+import { sign } from 'jsonwebtoken';
+
+export const generateToken = (payload, type = 'access') => {
+  const secret =
+    type === 'refresh'
+      ? process.env.JWT_REFRESH_SECRET
+      : process.env.JWT_ACCESS_SECRET;
+
+  const expiresIn =
+    type === 'refresh'
+      ? process.env.JWT_REFRESH_EXPIRES_IN
+      : process.env.JWT_ACCESS_EXPIRES_IN;
+
+  return sign(payload, secret, { expiresIn });
+};
