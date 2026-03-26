21f43e9069cf285cf2d88d82fa338cb7c73e039a
DrAmanJy <dramanjy0770@gmail.com>
2026-03-26 13:15:09 +0530

    chore(api): remove unused paths from jsconfig and integrate error handling middleware in app

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -1,5 +1,6 @@
 import e from 'express';
 import authRoutes from './modules/auth/auth.routes.js';
+import { errorHandler } from './shared/middleware/error.middleware.js';
 
 const app = e();
 
@@ -8,5 +9,7 @@ app.use(e.json({ limit: '1kb' }));
 app.get('/', (req, res) => res.json({ message: 'hello world' }));
 
 app.use('/api/v1/auth', authRoutes);
+
+app.use(errorHandler);
 
 export default app;
