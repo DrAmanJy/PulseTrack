3ea1721bdc837f69709a25d20f6f50ced87855e2
DrAmanJy <dramanjy0770@gmail.com>
2026-03-27 13:48:26 +0530

    feat(error): add AppError class for improved error handling and implement 404 error middleware

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -1,6 +1,7 @@
 import e from 'express';
 import authRoutes from './modules/auth/auth.routes.js';
 import { errorHandler } from './shared/middleware/error.middleware.js';
+import { AppError } from './shared/errors/AppError.js';
 
 const app = e();
 
@@ -9,6 +10,10 @@ app.use(e.json({ limit: '1kb' }));
 app.get('/', (req, res) => res.json({ message: 'hello world' }));
 
 app.use('/api/v1/auth', authRoutes);
+
+app.use((req, res, next) =>
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)),
+);
 
 app.use(errorHandler);
 
