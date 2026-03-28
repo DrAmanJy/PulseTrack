7da4ad402d952518057d117eda2130e0c455669a
DrAmanJy <dramanjy0770@gmail.com>
2026-03-28 16:17:17 +0530

    feat(compression): integrate compression middleware to optimize response sizes and improve performance

--- apps/api/src/app.js
+++ apps/api/src/app.js
@@ -1,13 +1,22 @@
-import e from 'express';
-import authRoutes from './modules/auth/auth.routes.js';
-import { errorHandler } from './shared/middleware/error.middleware.js';
-import { AppError } from './shared/errors/AppError.js';
-import { globalLimiter } from './shared/middleware/rateLimiter.middleware.js';
+import express from 'express';
+import helmet from 'helmet';
+import compression from 'compression';
+
 import { env } from './config/env.js';
 import { httpLogger } from './shared/logger/httpLogger.js';
-import helmet from 'helmet';
+import { AppError } from './shared/errors/AppError.js';
 
-const app = e();
+import { globalLimiter } from './shared/middleware/rateLimiter.middleware.js';
+import { errorHandler } from './shared/middleware/error.middleware.js';
+
+// ==========================================
+// 4. ROUTERS
+// ==========================================
+import authRoutes from './modules/auth/auth.routes.js';
+
+const app = express();
+
+
 app.use(
   helmet({
     crossOriginResourcePolicy: { policy: 'cross-origin' },
@@ -15,18 +24,42 @@ app.use(
   }),
 );
 app.disable('x-powered-by');
+
+// Logging
 app.use(httpLogger);
-app.use(e.json({ limit: '1kb' }));
 
-app.use(globalLimiter);
-app.get('/', (req, res) => res.json({ message: 'hello world' }));
-
-app.use(`'${env.API_PREFIX}/auth`, authRoutes);
-
-app.use((req, res, next) =>
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404)),
+app.use(
+  compression({
+    threshold: 1024, // Only compress responses > 1KB
+    filter: (req, res) => {
+      if (req.headers['x-no-compression']) {
+        return false;
+      }
+      return compression.filter(req, res);
+    },
+  }),
 );
 
+app.use(express.json({ limit: '10kb' }));
+app.use(express.urlencoded({ extended: true, limit: '10kb' }));
+
+app.use(globalLimiter);
+
+app.get('/', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    message: 'Welcome to the PulseTrack API',
+  });
+});
+
+app.use(`${env.API_PREFIX}/auth`, authRoutes);
+
+// Catch-all for unhandled routes (404)
+app.use((req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+// Global Error Handler
 app.use(errorHandler);
 
 export default app;
