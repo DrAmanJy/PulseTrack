1794d52b99551b5f7670fe6f0521e3a4ed6c8975
DrAmanJy <dramanjy0770@gmail.com>
2026-03-29 23:04:22 +0530

    feat(activity): implement activities controller with CRUD operations for managing user activities

---
+++ apps/api/src/modules/activities/activities.controller.js
@@ -0,0 +1,85 @@
+import { AppError } from '../../shared/errors/AppError.js';
+import Activity from './activities.model.js';
+
+export const getActivities = async (req, res) => {
+  const { id: userId } = req.user;
+  const activities = await Activity.find({ userId }).sort({ date: -1 }).lean();
+
+  res.status(200).json({
+    status: 'success',
+    results: activities.length,
+    message: 'Activities fetched successfully',
+    data: { activities },
+  });
+};
+
+export const getActivity = async (req, res) => {
+  const { id: activityId } = req.params;
+  const { id: userId } = req.user;
+
+  const activity = await Activity.findOne({ _id: activityId, userId }).lean();
+
+  if (!activity) throw new AppError('Activity not found or unauthorized', 404);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Activity fetched successfully',
+    data: { activity },
+  });
+};
+
+export const createActivity = async (req, res) => {
+  const { id: userId } = req.user;
+
+  const newActivity = await Activity.create({
+    ...req.body,
+    userId,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    message: 'Activity saved',
+    data: { activity: newActivity },
+  });
+};
+
+export const updateActivity = async (req, res) => {
+  const { id: activityId } = req.params;
+  const { id: userId } = req.user;
+
+  const updatedActivity = await Activity.findOneAndUpdate(
+    { _id: activityId, userId },
+    { $set: req.body },
+    {
+      new: true,
+      runValidators: true,
+      lean: true,
+    },
+  );
+
+  if (!updatedActivity) throw new AppError('Activity not found or unauthorized', 404);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Activity successfully updated',
+    data: { activity: updatedActivity },
+  });
+};
+
+export const deleteActivity = async (req, res) => {
+  const { id: activityId } = req.params;
+  const { id: userId } = req.user;
+
+  const deletedActivity = await Activity.findOneAndDelete({
+    _id: activityId,
+    userId,
+  });
+
+  if (!deletedActivity) throw new AppError('Activity not found or unauthorized', 404);
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Activity successfully deleted',
+    data: { id: deletedActivity._id },
+  });
+};
