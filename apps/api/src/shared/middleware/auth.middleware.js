48b5b68048c56c4e2da95f26afda4375bffb5739
DrAmanJy <dramanjy0770@gmail.com>
2026-03-29 23:04:04 +0530

    feat(auth): implement authentication middleware to verify JWT tokens and manage user access

---
+++ apps/api/src/shared/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from 'jsonwebtoken';
+import { AppError } from '../errors/AppError.js';
+import User from '../../modules/auth/auth.model.js';
+import { env } from '../../config/env.js';
+
+export const requireAuth = async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith('Bearer '))
+    throw new AppError('Not authorized, no token provided', 401);
+
+  const accessToken = authHeader.split(' ')[1];
+  if (!accessToken) {
+    throw new AppError('Invalid token format', 401);
+  }
+
+  let decoded;
+  try {
+    decoded = jwt.verify(accessToken, env.JWT_ACCESS_SECRET);
+  } catch (error) {
+    throw new AppError('Invalid or expired access token', 401);
+  }
+
+  const user = await User.findById(decoded.id);
+
+  if (!user) throw new AppError('The user belonging to this token no longer exists', 401);
+
+  if (!user.isActive) throw new AppError('This user account is inactive', 403);
+
+  req.user = user;
+
+  next();
+};
