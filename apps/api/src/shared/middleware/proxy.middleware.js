d214281ae296362f530e1b8a2760e2c3f514ed1e
DrAmanJy <dramanjy0770@gmail.com>
2026-05-11 16:22:04 +0530

    feat(proxy): add http-proxy-middleware for authentication service proxying with custom headers and error handling

---
+++ apps/api/src/shared/middleware/proxy.middleware.js
@@ -0,0 +1,26 @@
+import { createProxyMiddleware } from 'http-proxy-middleware';
+import { env } from '../../config/env.js';
+
+export const authProxy = createProxyMiddleware({
+  pathFilter: ['/auth', '/user'],
+
+  target: env.AUTH_SERVICE_URL,
+  changeOrigin: true,
+
+  on: {
+    proxyReq: (proxyReq, req, res) => {
+      proxyReq.setHeader('x-target-audience', env.APP_NAME);
+      proxyReq.setHeader('x-forwarded-for', req.ip || req.connection.remoteAddress);
+
+      console.log(
+        `[Gateway] Proxying Full Path: ${req.originalUrl} -> ${env.AUTH_SERVICE_URL}${req.url}`,
+      );
+    },
+    error: (err, req, res) => {
+      res.status(502).json({
+        success: false,
+        message: 'Auth Service unreachable.',
+      });
+    },
+  },
+});
